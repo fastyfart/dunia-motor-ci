0309d9b0c03f619c66ca324f60442433ce5e434e
fastyfart <asfastasfart@gmail.com>
2014-12-26 01:32:28 +0700

    deleted unecessary

--- application/controllers/category.php
+++ application/controllers/category.php
@@ -29,7 +29,7 @@ class Category extends Master
 			$products = $this->product_model->product_by_cat($_GET['brand'], $_GET['cat']);
 			$categories = $this->category_model->get_all();
 		}
-		
+
 		$data['brands'] = $this->brand_model->get_all();
 		$data['categories'] = (isset($categories)) ? $categories : $record->category;
 		$data['products'] = (isset($products)) ? $products : $record->product;
